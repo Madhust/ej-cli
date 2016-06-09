d5617e41fa81ae858966a4ec320e29c353190876
Madhu <madhusudhananit@gmail.com>
2016-06-09 16:16:55 +0530

    Resolved file exist failed case
    Resolved file exist failed case

--- bash_scripts/project.bat
+++ bash_scripts/project.bat
@@ -2,17 +2,17 @@
 
 set pname=%1
 set ejversion=%2
-set ejpath="C:\Program Files (x86)\Syncfusion\Essential Studio\%ejversion%\JavaScript\assets""
+set ejpath=C:\Program Files (x86)\Syncfusion\Essential Studio\%ejversion%\JavaScript\assets
 
-if exist %ejpath% (    
+if exist "%ejpath%" (    
     md %pname%
     cd %pname% 
     md %ejversion%
     cd %ejversion%
     md scripts
     md themes 
-    xcopy /s /z "%ejpath%\scripts" scripts
-    xcopy /s /z "%ejpath%\css" themes
+    xcopy /s /z /y "%ejpath%\scripts" scripts
+    xcopy /s /z /y "%ejpath%\css" themes
 ) else (
-echo Specified version is not found in this machine, please install %ejversion% and try again
+echo Specified version is not found in this machine, please install %ejversion% or whether you entered the correct version and try again
 )
